1e2e8dedd76794f867f9103aa8c0ec98c1471fc6
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-05-28 01:32:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+/*1) should have a `totalBatteries` variable
+  2) should have a number as a result
+  3) should have made the sum of all the assembled batteries */
+
+  function sum(accumulator, currentValue) {
+    return accumulator + currentValue;
+  }
+  
+  const totalBatteries = batteryBatches.reduce(sum, 0);
+  
+  console.log(totalBatteries);
+  //or
+  /*const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  console.log(totalBatteries);*/
+ 
